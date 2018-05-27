3129488e2f9fb50a912285cf26fd0f44293438c0
Zhmayev Yaroslav <salaros@yandex.ru>
2018-05-27 22:49:48 +0300

    adding CSS minification and source map creation via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,30 @@ var gulp = require('gulp'),
     replace = require('gulp-replace'),
     flatten = require('gulp-flatten'),
     plumber = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    clone = require('gulp-clone'),
+    cssnano = require('gulp-cssnano');
 
 var tasks = require('./semantic-ui/tasks/config/tasks.js'),
     settings = tasks.settings;
 
 gulp.task('less:compile', function () {
-  return gulp.src('./less/*.less')
+  var stream = gulp.src('./less/*.less')
     .pipe(plumber(settings.plumber.less))
+    .pipe(sourcemaps.init())
     .pipe(less(settings.less))
     .pipe(autoprefixer(settings.prefix))
     .pipe(replace(/((.*?)\n)+\/\* Semantic UI Addons \*\//gmi, ''))
     .pipe(flatten())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(cssnano())
+    .pipe(rename({
+        suffix: ".min",
+    }))
+    .pipe(sourcemaps.write("./", {
+        addComment: false
+    }))
     .pipe(gulp.dest('./dist/'));
 });
