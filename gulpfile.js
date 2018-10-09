c2aef84120126a3614db2bfc186c8fd34426bf04
salaros <salaros@yandex.ru>
2018-10-09 16:31:55 +0300

    compiling add-ons from LESS to CSS, refreshing SUI CSS too

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,37 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rename = require('gulp-rename'),
-    cssnano = require('gulp-cssnano'),
-    concat = require('gulp-concat'),
-    exec = require('gulp-exec');
-
-var tasks = require('./semantic-ui/tasks/config/tasks.js'),
-    settings = tasks.settings;
+const gulp = require('gulp'),
+      sourcemaps = require('gulp-sourcemaps'),
+      rename = require('gulp-rename'),
+      cssnano = require('gulp-cssnano'),
+      less = require('gulp-less'),
+      concat = require('gulp-concat'),
+      strip = require('gulp-strip-css-comments'),
+      newLinesFix = require('gulp-remove-empty-lines');
 
 // Default task
-gulp.task('default', [ 'css:concat' ]);
+gulp.task('default', [ 'css:concat', 'less:compile' ]);
+
+gulp.task('less:compile', function () {
+    return gulp.src('./addons/!index.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(strip())
+        .pipe(newLinesFix())
+        .pipe(rename({
+            basename: "addons",
+        }))
+        .pipe(gulp.dest('./dist/components'))
+        .pipe(cssnano())
+        .pipe(rename({
+            suffix: ".min",
+        }))
+        .pipe(sourcemaps.write("./", {
+            addComment: false
+        }))
+        .pipe(gulp.dest('./dist/components'));
+});
 
 gulp.task('css:concat', [ 'fonts:copy' ], function () {
-    gulp.src([
+    return gulp.src([
             './node_modules/lato-font/css/lato-font.css',
             './dist/semantic.css'
         ])
@@ -31,6 +50,6 @@ gulp.task('css:concat', [ 'fonts:copy' ], function () {
 
 gulp.task('fonts:copy', function (cb) {
     return gulp.src('node_modules/lato-font/fonts/**/*')
-                .pipe(gulp.dest('./fonts/'));
+        .pipe(gulp.dest('./fonts/'));
 
 });
